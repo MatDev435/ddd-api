16b33af513cde215e83ce5f5ba8b0d9756e97d3f
MatDev435 <matheuspsalmazo2@gmail.com>
2024-05-09 14:33:39 -0300

    Fundamentos do DDD -- Getters & Setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,7 @@
 import { Entity } from '../../core/entities/entity'
 import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 import { Optional } from '../../core/types/optional'
+import dayjs from 'dayjs'
 
 interface AnswerProps {
   questionId: UniqueEntityID
@@ -11,8 +12,41 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
   get content() {
     return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.props.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.props.createdAt, 'days') <= 3
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
   }
 
   static create(
