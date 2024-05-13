acf8e5989632d4f190c35c3551a2388386e7a68c
MatDev435 <matheuspsalmazo2@gmail.com>
2024-05-13 13:44:28 -0300

    DDD & Clean Architecture - Casos de uso: Comentar em uma resposta

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryAnswersRepository } from 'tests/repositories/in-memory-answers-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+import { InMemoryAnswerCommentsCommentsRepository } from 'tests/repositories/in-memory-answer-comments-repository'
+import { makeAnswer } from 'tests/factories/make-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerComments: InMemoryAnswerCommentsCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on Answer Use Case', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerComments = new InMemoryAnswerCommentsCommentsRepository()
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerComments,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      authorId: 'author-01',
+      answerId: answer.id.toString(),
+      content: 'Test Comment',
+    })
+
+    expect(inMemoryAnswerComments.items[0]).toMatchObject({
+      content: 'Test Comment',
+    })
+  })
+})
